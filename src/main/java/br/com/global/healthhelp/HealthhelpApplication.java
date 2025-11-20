a89210e498ae3dcc73bde83a404c630f0833cf3b
CauaMachad0 <caua.marcelo.machado@outlook.com>
2025-11-19 21:12:53 -0300

    Subindo Projeto

---
+++ src/main/java/br/com/global/healthhelp/HealthhelpApplication.java
@@ -0,0 +1,13 @@
+package br.com.global.healthhelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthhelpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthhelpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/global/healthhelp/HealthhelpApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.global.healthhelp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthhelpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
